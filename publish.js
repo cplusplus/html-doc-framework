ba8de1b804c894ceea1b506d75294a92e5e120c6
Jeffrey Yasskin <jyasskin@gmail.com>
2014-07-05 15:32:54 -0700

    Teach the Publish button to generate a JSON index suitable for <cxx-foreign-index>.

--- publish.js
+++ publish.js
@@ -139,6 +139,22 @@ limitations under the License.
         });
     }
 
+    function buildSectionIndex(doc) {
+        var result = {};
+        doc.querySelectorAll('cxx-clause,cxx-section').array().forEach(function(section) {
+            if (!section.id) {
+                console.warn(section, 'is missing its id.');
+                return;
+            }
+            if (!section.sec_num || section.sec_num == '?') {
+                console.warn(section, 'is missing its number');
+                return;
+            }
+            result[section.id] = section.sec_num;
+        });
+        return result;
+    }
+
     Polymer('cxx-publish-button', {
         publishing: false,
         flattenedBlob: null,
@@ -152,12 +168,14 @@ limitations under the License.
             if (this.source) {
                 source = '<!-- Sources at ' + this.source + ' -->\n';
             }
+            var sectionIndex = buildSectionIndex(document);
             copyPromise.then(function(copy) {
                 var published = new Blob(['<!DOCTYPE html>\n',
                                           source,
                                           copy.documentElement.outerHTML],
                                          {type: 'text/html'});
                 this.flattenedBlob = URL.createObjectURL(published);
+                this.sectionIndex = encodeURI('data:application/json,' + JSON.stringify(sectionIndex));
             }.bind(this)).catch(function(e) {
                 console.error(e);
                 this.publishing = false;
